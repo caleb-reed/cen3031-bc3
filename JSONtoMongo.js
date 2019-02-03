1cc5a295ab5298948c0805e7a629697eb42d2240
Caleb Reed <calebreed@ufl.edu>
2019-02-03 00:01:50 -0500

    Final Working Version

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,14 +7,38 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+    listings = require('./listings.json');
+
 
 /* Connect to your database */
+mongoose.connect(config.db.uri);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+for (var i = 0; i < listings.entries.length; i++) {
+    //Creates a new listing
+    var newListing = Listing({
+        name: listings.entries[i].name,
+        code: listings.entries[i].code,
+        address: listings.entries[i].address
+    });
+    if (listings.entries[i].coordinates)
+    {
+        newListing.coordinates.latitude = listings.entries[i].coordinates.latitude;
+        newListing.coordinates.longitude = listings.entries[i].coordinates.longitude;
+        }
+    
+    //save the listing
+    newListing.save(function (err) {
+        if (err) throw err;
+
+        console.log('Listing created!');
+    });
+
+}
 
 
 /* 
--- ListingSchema.js
+++ ListingSchema.js
@@ -4,12 +4,32 @@ var mongoose = require('mongoose'),
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
+    code: { type: String, required: true, unique:true},
+    name: { type: String, required: true},
+    address: String,
+    coordinates: {
+        latitude: Number,
+        longitude: Number
+    },
+    created_at: Date,
+    updated_at: Date
+
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
-listingSchema.pre('save', function(next) {
-  /* your code here */
+listingSchema.pre('save', function (next) {
+    //Source for below function: https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+    // get the current date
+    var currentDate = new Date();
+
+    // change the updated_at field to current date
+    this.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    if (!this.created_at)
+        this.created_at = currentDate;
+
+    next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
